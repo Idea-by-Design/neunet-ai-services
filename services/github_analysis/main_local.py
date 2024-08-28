2a769b69f5161a1e64c3abfdd6cbb207fbd6d081
VedikaSrivastava <vedikas3012@gmail.com>
2024-08-27 23:35:25 -0400

    added github analysis local file

---
+++ services/github_analysis/main_local.py
@@ -0,0 +1,16 @@
+from services.github_analysis.analyze_github import analyze_github_profile
+
+def main():
+    # Provide the GitHub identifier (username or URL) and candidate email for testing
+    github_identifier = "VedikaSrivastava"
+    candidate_email = "vedikas3012@gmail.com"
+
+    # Perform the analysis
+    analysis_data = analyze_github_profile(github_identifier, candidate_email)
+
+    # Print the analysis result
+    print("Analysis Result:")
+    print(analysis_data)
+
+if __name__ == "__main__":
+    main()
