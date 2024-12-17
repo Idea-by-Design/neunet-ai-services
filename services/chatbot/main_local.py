0d372a2a234829ba80c12347dc74d41c51b0fba5
Hemant Kumar Singh <iamhks14@gmail.com>
2024-12-16 20:06:22 -0500

    chatbot: updated files and folders.
    Co-authored-by: Vedika Srivastava <vedikas3012@gmail.com>

--- services/chatbot/main_local.py
+++ services/chatbot/main_local.py
@@ -1,11 +1,10 @@
-from services.chatbot.multiagent_assistant import initiate_chat
+from services.chatbot.multiagent_assistant_copy import initiate_chat
 
 
 def main():
     """
     Entry point for the script.
     """
-    
     initiate_chat()
 
 if __name__ == "__main__":
